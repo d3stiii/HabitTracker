e91b05ef9dd5cb413f5826f4ebb7ded069fbe8b8
x1mine <emptybrawl@gmail.com>
2023-07-01 21:14:06 +0300

    clean up

--- HabitTracker.UI/App.xaml.cs
+++ HabitTracker.UI/App.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace HabitTracker.UI
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application { }
-}
+namespace HabitTracker.UI;
+
+public partial class App : Application { }
